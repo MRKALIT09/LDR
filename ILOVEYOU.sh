1a1fb57d05f8eb26d0f273db505741abfc761223
MedanXploit12 <75316440+MRKALIT09@users.noreply.github.com>
2021-01-23 16:50:47 +0700

    Add files via upload

---
+++ ILOVEYOU.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+echo
+echo
+echo
+echo
+     echo $me "                  [$ku∆$me] $cy MR-KALIT $me[$ku∆$me]                 "
+     echo $me "         $me[$ku∆$me]$cy https://github.com/MRKALIT09$me [$ku∆$me]            "
+
+
+read -p  "        MASUKAN NAMA ORG YG LU SUKA >< :" nama
+read -p  "        JUMLAH KATA² NYA               :"  jumlah
+echo $cy "        SEDANG MENYIAPKAN KATA KATA NYA"
+sleep 3
+while [ true ]; do
+echo $me " BUAT KAMU $nama  KAMU MAU $i GA JADI IBU DARI ANAK KITA NANTI ><:) "
+done
+
